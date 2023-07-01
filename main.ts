9c95d6bb0f8c9a6bc73627a2cc433ad637c71645
addison5017 <63955619+addison5017@users.noreply.github.com>
2023-07-01 16:15:59 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,35 @@
+basic.forever(function () {
+    mbit_小車類.CarCtrl(mbit_小車類.CarState.Car_Run)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    mbit_小車類.CarCtrl(mbit_小車類.CarState.Car_Back)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    mbit_小車類.CarCtrl(mbit_小車類.CarState.Car_Left)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    mbit_小車類.CarCtrl(mbit_小車類.CarState.Car_Right)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
